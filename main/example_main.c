4f5a04f32dac5d62725859182693d6250f1d866c
Luiz Henrique <henriquetiano_2010@hotmail.com>
2024-08-18 14:37:34 -0300

    consertando máquina de estados

--- main/example_main.c
+++ main/example_main.c
@@ -17,30 +17,54 @@ Quaternion quaternion;
 Quaternion quaternionToGetTest;
 EulerAngle eulerAngle;
 
+/*Estados do sistema*/
+typedef enum {
+    GET_IMU_DATA = 0, CALCULATE_QUATERNIONS, CALCULATE_EULER_ANGLES
+} States;
+
 void app_main() {
   imu_init();
+
+  static States state = GET_IMU_DATA;
+  static const int quantStates = 3;//variavel que informa a quantidade de estados
+
   while (true){
-    //exemplos sensor_imu
-    get_acceleration_data(&accelerationMeasures);
-    get_gyroscope_data(&gyroscopeMeasures);
+    for (int i = 0; i < quantStates; i++){
+      switch(state) {
+        case GET_IMU_DATA:
+          get_acceleration_data(&accelerationMeasures);
+          get_gyroscope_data(&gyroscopeMeasures);
+          get_imu_data(&gyroAcellMeasures);
+          state = CALCULATE_QUATERNIONS;
 
-    ESP_LOGI(TAG_SENSOR_IMU, "acce_x:%.2f, acce_y:%.2f, acce_z:%.2f", accelerationMeasures.x, accelerationMeasures.y, accelerationMeasures.z);
-    ESP_LOGI(TAG_SENSOR_IMU, "gyro_x:%.2f, gyro_y:%.2f, gyro_z:%.2f\n", gyroscopeMeasures.x, gyroscopeMeasures.y, gyroscopeMeasures.z);
-    
-    //exemplos imu_tools
-    get_imu_data(&gyroAcellMeasures);
-    calculate_quaternion(&gyroAcellMeasures, &quaternion);
-    quaternion_to_euler(&quaternion, &eulerAngle);
-    get_quaternion(&quaternionToGetTest);
+          //Saída dos dados capturados
+          ESP_LOGI(TAG_SENSOR_IMU, "acce_:%.2f, acce_y:%.2f, acce_z:%.2f", accelerationMeasures.x, accelerationMeasures.y, accelerationMeasures.z);
+          ESP_LOGI(TAG_SENSOR_IMU, "gyro_x:%.2f, gyro_y:%.2f, gyro_z:%.2f\n", gyroscopeMeasures.x, gyroscopeMeasures.y, gyroscopeMeasures.z);
+          
+          ESP_LOGI(TAG_IMU_TOOLS, "acce_x:%.2f, acce_y:%.2f, acce_z:%.2f", gyroAcellMeasures.accel_x, gyroAcellMeasures.accel_y, gyroAcellMeasures.accel_z);
+          ESP_LOGI(TAG_IMU_TOOLS, "gyro_x:%.2f, gyro_y:%.2f, gyro_z:%.2f\n", gyroAcellMeasures.gyro_x, gyroAcellMeasures.gyro_y, gyroAcellMeasures.gyro_z);
+          break;
 
-    ESP_LOGI(TAG_IMU_TOOLS, "acce_x:%.2f, acce_y:%.2f, acce_z:%.2f", gyroAcellMeasures.accel_x, gyroAcellMeasures.accel_y, gyroAcellMeasures.accel_z);
-    ESP_LOGI(TAG_IMU_TOOLS, "gyro_x:%.2f, gyro_y:%.2f, gyro_z:%.2f\n", gyroAcellMeasures.gyro_x, gyroAcellMeasures.gyro_y, gyroAcellMeasures.gyro_z);
-    
-    ESP_LOGI(TAG_IMU_TOOLS, "quat_w:%.2f, quat_x:%.2f, quat_y:%.2f, quat_z:%.2f", quaternion.w, quaternion.x, quaternion.y, quaternion.z);
-    ESP_LOGI(TAG_IMU_TOOLS, "quat_get_w:%.2f, quat_get_x:%.2f, quat_get_y:%.2f, quat_get_z:%.2f", quaternionToGetTest.w, quaternionToGetTest.x, quaternionToGetTest.y, quaternionToGetTest.z);
-    ESP_LOGI(TAG_IMU_TOOLS, "pitch:%.2f, yaw:%.2f, roll:%.2f\n", eulerAngle.pitch, eulerAngle.yaw, eulerAngle.roll);
+        case CALCULATE_QUATERNIONS:
+          calculate_quaternion(&gyroAcellMeasures, &quaternion);
+          get_quaternion(&quaternionToGetTest);
+          state = CALCULATE_EULER_ANGLES;
 
-    vTaskDelay(2000 / portTICK_PERIOD_MS);
+          //Saída dos dados capturados
+          ESP_LOGI(TAG_IMU_TOOLS, "quat_w:%.2f, quat_x:%.2f, quat_y:%.2f, quat_z:%.2f", quaternion.w, quaternion.x, quaternion.y, quaternion.z);
+          ESP_LOGI(TAG_IMU_TOOLS, "quat_get_w:%.2f, quat_get_x:%.2f, quat_get_y:%.2f, quat_get_z:%.2f", quaternionToGetTest.w, quaternionToGetTest.x, quaternionToGetTest.y, quaternionToGetTest.z);
+          break;
+          
+        case CALCULATE_EULER_ANGLES:
+          quaternion_to_euler(&quaternion, &eulerAngle);
+          state = GET_IMU_DATA;
+
+          //Saída dos dados capturados
+          ESP_LOGI(TAG_IMU_TOOLS, "pitch:%.2f, yaw:%.2f, roll:%.2f\n", eulerAngle.pitch, eulerAngle.yaw, eulerAngle.roll);
+          break;
+      }
+    }
+    vTaskDelay(2000 / portTICK_PERIOD_MS); //delay para evitar de poluir o terminal com muitos prints
   }
   
 }
